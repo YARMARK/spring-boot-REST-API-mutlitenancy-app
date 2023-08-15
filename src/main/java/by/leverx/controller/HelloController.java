fbd311026b50b50589039da6c71aa9b5f756a37f
Ярослав Марков <@iaroslav_markov>
2023-08-15 16:02:13 +0300

    Add Logging service

--- src/main/java/by/leverx/controller/HelloController.java
+++ src/main/java/by/leverx/controller/HelloController.java
@@ -1,6 +1,8 @@
 package by.leverx.controller;
 
 import com.sap.cloud.security.xsuaa.token.Token;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "")
 public class HelloController {
 
+  public static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
   @GetMapping(path = "")
   public ResponseEntity<String> readAll(@AuthenticationPrincipal Token token) {
 //    if (!token.getAuthorities().contains(new SimpleGrantedAuthority("Display"))) {
 //      throw new NoAuthorizedException("This operation requires \"Display\" scope");
 //    }
+    logger.info("HELLO-CONTROLLER");
     return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
   }
 }
